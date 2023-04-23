8bc32ff831e586440b6b232b10b02d72da5eb4b5
Winston Kinoti <prog.kinoti@gmail.com>
2023-04-23 15:49:20 +0300

    feat: add django-debug-toolbar plugin

--- djblogger/settings/local.py
+++ djblogger/settings/local.py
@@ -1,3 +1,11 @@
 from .base import *
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
